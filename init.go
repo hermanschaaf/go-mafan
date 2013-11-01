bddd961f8afa26ba80e42c4a46981c5f20159053
Herman Schaaf <hermanschaaf@gmail.com>
2013-11-01 21:23:22 +0900

    Read data file

--- init.go
+++ init.go
@@ -1,5 +1,21 @@
 package mafan
 
-func init() {
+import (
+	"io/bufio"
+	"strings"
+)
 
+var TrieRoot *Trie = &Trie{map[string]*Trie{}, "", []string{}}
+
+func init() {
+	file, _ := os.Open("data/dict.txt.big")
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		parts := strings.Split(scanner.Text(), " ")
+		fmt.Println(parts[0])
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
 }
