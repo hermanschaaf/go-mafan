98922b5c27629dd1a47b058dc475195c336503c3
Herman Schaaf <hermanschaaf@gmail.com>
2013-11-13 21:17:34 +0900

    Get rid of gocheck

--- split_test.go
+++ split_test.go
@@ -1,17 +1,18 @@
 package mafan
 
 import (
-	. "launchpad.net/gocheck"
 	"strings"
 	"testing"
 )
 
-func Test(t *testing.T) { TestingT(t) }
+func TestSplit(t *testing.T) {
+	split := strings.Join(Split("上海十大接吻聖地"), " ")
+	expected := "上海 十大 接吻 聖地"
+	if split != expected {
+		t.Errorf("%s != %s", split, expected)
+	}
+}
 
-type SplitTestSuite struct{}
+func BenchmarkSetup(b *testing.B) {
 
-var _ = Suite(&SplitTestSuite{})
-
-func (s *SplitTestSuite) TestSplit(c *C) {
-	c.Check(strings.Join(Split("上海十大接吻聖地"), " "), Equals, "上海 十大 接吻 聖地")
 }
